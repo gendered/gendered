0913494c8f1ede7285d0ca68c9af99ecd4a1f68c
Omayeli Arenyeka <aomayeli@gmail.com>
2018-08-08 16:56:59 -0400

    Word list shouldnt display as one long line

--- client/src/components/WordList.vue
+++ client/src/components/WordList.vue
@@ -1,21 +1,18 @@
-<template>
-  <div class="word-list">
-    <ul>
-       <li v-for="(value, key) in words">
-          <span>{{key}}</span>
-          <ul v-for="word in value">
-            <li>
-              <a :href="'./words/' + word.word" :class="'word ' + word.gender">{{word.word}}</a>
-              <ul v-for="tag in word.tags">
-              <span><i>{{tag}}</i></span>
-              </ul>
-            </li>
-          </ul>
-       </li>
-     </ul>
-  </div>
+  <template>
+  <ul class="word-list">
+     <li class="letter-list" v-for="(value, key) in words">
+        <span>{{key}}</span>
+        <ul v-for="word in value">
+          <li>
+            <a :href="'./words/' + word.word" :class="'word ' + word.gender">{{word.word}}</a>
+            <ul v-for="tag in word.tags">
+            <span><i>{{tag}}</i></span>
+            </ul>
+          </li>
+        </ul>
+     </li>
+   </ul>
 </template>
-
 <script>
   const API = 'http://localhost:3000/api/words';
   export default {
