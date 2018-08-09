81c8dd77dfda5009e96d951b147acb96f83ecfb4
Omayeli Arenyeka <aomayeli@gmail.com>
2018-08-09 16:41:19 -0400

    Moving filter styles from home to filter component

--- client/src/components/FilterPanel.vue
+++ client/src/components/FilterPanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul v-if="options && options.length">
+  <ul class="filter-list" v-if="options && options.length">
      <li class="filter" v-for="option in options">
         <input v-bind:class="{ active: option.active }" type="submit" @click="clicked(option)" :value="option.type" />
      </li>
@@ -25,8 +25,33 @@ export default {
 };
 </script>
 <style>
-  .filter input.active {
+  .filter-list {
+    padding: 0;
+    margin: 0;
+    display: inline-flex;
+
+    .filter {
+      margin: 0;
+      padding: 1rem;
+    }
+  }
+
+  @mixin active-filter {
     color: white;
     background-color: black;
+  }
+
+  .filter {
+    input {
+      border-radius: 0;
+      font-size: 1rem;
+      border: 1px solid black;
+      padding: 0.5rem 1rem;
+      transition: all 0.1s;
+    }
+
+    input.active, input:hover {
+     @include active-filter;
+    }
   }
 </style>
