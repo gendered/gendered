4eb4a79964a055e0f346126103e84a58adb82ce0
Omayeli Arenyeka <aomayeli@gmail.com>
2018-08-08 22:24:14 -0400

    Words...

--- server/boot/words.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
-  var data = require("../all.json");
-  var Word = app.models.Word;
-  app.dataSources.db.automigrate('Word', function(err) {
-    Word.create(data, function(err, instances) {
-    });
-  });
-};
-
