c0d47d99f9523dced2d328f49abc5e102bc5e5e6
Omayeli Arenyeka <aomayeli@gmail.com>
2018-08-08 22:46:53 -0400

    Resetting

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,12 +1,8 @@
 'use strict';
 
 module.exports = function(server) {
-	// Install a `/` route that returns server status
-	var router = server.loopback.Router();
-	router.get('/', server.loopback.status());
-	server.use(router);
-	var memory = loopback.createDataSource({
-	  connector: loopback.Memory,
-	  file: "mydata.json"
-	});
+  // Install a `/` route that returns server status
+  var router = server.loopback.Router();
+  router.get('/', server.loopback.status());
+  server.use(router);
 };
