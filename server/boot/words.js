33a20c04706a4d1b6c0f76b7a67e891d3cc7905f
Omayeli Arenyeka <aomayeli@gmail.com>
2018-08-10 17:53:49 -0400

    More words

--- server/boot/words.js
+++ server/boot/words.js
@@ -3,12 +3,6 @@ module.exports = function(app) {
 	var Word = app.models.Word;
 	data.forEach((entry) => {
 		let word = entry['word'];
-		Word.findOne({ where: { 'word': word }}, function(err, record) {
-			if(!record) {
-				Word.upsert(entry, function(err, instance) {
-					// console.log(instance);
-				});
-			}
-	   });
+		Word.upsertWithWhere({ 'word': word }, entry, function(err, record) {});
 	});
 };
