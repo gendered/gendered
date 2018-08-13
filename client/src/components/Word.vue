1a577df30418b1e40b5e8a84e85aa20ad5757f15
Omayeli Arenyeka <aomayeli@gmail.com>
2018-08-13 14:45:38 -0400

    Small css fixes

--- client/src/components/Word.vue
+++ client/src/components/Word.vue
@@ -3,7 +3,7 @@
     <h1>{{ entry.word }}</h1>
     <p>{{ entry.definition }}</p>
     <ul class="word-set" v-for="syn in entry.syns">
-      <li v-if="syn != entry.word"><a :href="'./words/' + syn">{{syn}}</a></li>
+      <li v-if="syn != entry.word"><a :href="syn">{{syn}}</a></li>
     </ul>
     <ul class="tag-list" v-for="tag in entry.tags">
       <li>{{tag}}</li>
