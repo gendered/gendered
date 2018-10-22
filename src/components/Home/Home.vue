d78c14d68a7f2a323ddaf925241229d4e3e67a05
seancaat <seancaat@gmail.com>
2018-10-22 11:42:11 -0400

    aligned fixed search to the grid, added styles, hiding get random for now.. will change later

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
   	<aside>
-		<SearchFilter v-on:input="updateSearchText"></SearchFilter>
-	 	<input type="button" v-on:click="getRandom" value="Get Random Word" />
+		<SearchFilter v-on:input="updateSearchText" class="search"></SearchFilter>
+	 	<input class="random" type="button" v-on:click="getRandom" value="Get Random Word" />
 	 </aside>
 	<keep-alive>
 		<!-- list of words for each letter -->
 		<!-- contians 26 WordList components -->
 		<Main v-bind:words="filteredWords"></Main>
-	</keep-alive>	
+	</keep-alive>
 	<Footer v-on:filter="handleFilter"></Footer>
   </div>
 </template>
