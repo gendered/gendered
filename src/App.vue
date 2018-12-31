b39780309def09c03e71ef8b66d414a03365a4f9
Omayeli Arenyeka <aomayeli@gmail.com>
2018-12-31 08:44:40 -0800

    Adding fonts

--- src/App.vue
+++ src/App.vue
@@ -16,16 +16,6 @@
 import router from './router';
 export default {
   name: 'App',
-	mounted() {
-		(function() {
-				var trial = document.createElement('script');
-				trial.type = 'text/javascript';
-				trial.async = true;
-				trial.src = 'https://easy.myfonts.net/v2/js?sid=362(font-family=Romana+Std+Roman)&sid=363(font-family=Romana+Std+Bold)&key=43V6OV8XMY';
-				var head = document.getElementsByTagName("head")[0];
-				head.appendChild(trial);
-		})();
-	},
   methods: {
   	closeModal: function(e) {
   		const params = this.$route.params;
