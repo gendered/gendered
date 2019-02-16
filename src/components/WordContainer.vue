c78c006240ba1cd685b6f1647d760cd22a63a404
Omayeli Arenyeka <oarenyek@oarenyek-mn1.linkedin.biz>
2019-02-16 01:51:34 -0800

    Styling icons

--- src/components/WordContainer.vue
+++ src/components/WordContainer.vue
@@ -9,15 +9,16 @@
 			class="modal-link"
 		>
 			✕
-			<img src="../assets/imgs/close.svg" alt="Close modal"/>
 		</router-link>
 		<div class="w-80 w-60-ns center word-set-container">
 			<WordInfo v-if="entry" :entry="entry" />
 			<WordInfo v-if="equivalent" :entry="equivalent" />
 		</div>
 		<router-link :to="{ name: 'about' }" class="modal-link">
-			？
-			<img src="../assets/imgs/info.svg" alt="More information about project."/>
+			<img
+				src="../assets/imgs/info.svg"
+				alt="More information about project."
+			/>
 		</router-link>
 	</div>
 </template>
